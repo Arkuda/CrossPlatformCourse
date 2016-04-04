a6f84745c4fab90df15f7006c48ccbb32037d4e3
Andrey Kiryantsev <reyzor2142@gmail.com>
2016-04-04 08:12:40 +0400

    Занятие #4 - Firebase

--- CourseAPP/www/js/controllers.js
+++ CourseAPP/www/js/controllers.js
@@ -40,16 +40,51 @@ angular.module('starter.controllers', [])
     }, 1000);
   };
 })
+.controller('profileCtrl',function($scope) {
+  var userId;
+  var myFirebaseRef = new Firebase("https://cropdevtestapp.firebaseio.com/");
+  $scope.getUser = function(age){
+    myFirebaseRef.child("Users/").on("value", function(snapshot) {
+      var avalibleAges;
+      if(age != '' || age != undefined){
+      for (var i = 0; i < snapshot.val().length; i++) {
+        avalibleAges += " " + snapshot.val()[i].age;
+
+          if(snapshot.val()[i].age == age.toString())
+          {
+            userId = i;
+            $scope.user = snapshot.val()[i];
+            console.log($scope.user);
+            $scope.$apply();
+          }
+      }
+      }
+      if( $scope.user == undefined)
+      {
+        alert('Слушай, с таким возрастом нет. Но есть с таким:' + avalibleAges);
+      }
+    });
+  }
+  $scope.setAge = function(needAge){
+      if(userId == undefined){
+        alert('Set user first');
+      }
+      else {
+        myFirebaseRef.child('Users/' + userId + '/age').set(needAge);
+      }
+  }
+
+
+})
 
 .controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+  var myFirebaseRef = new Firebase("https://cropdevtestapp.firebaseio.com/");
+
+  myFirebaseRef.child("/").on("value", function(snapshot) {
+    $scope.playlists = snapshot.val();  // Alerts "San Francisco"
+    $scope.$apply();
+  });
+
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
